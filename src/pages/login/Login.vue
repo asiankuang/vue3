d266813302e083f6ccacbc0cef2c3bd317e2ba4c
asian.kuang <asian.kuang@cn.shijigruop.com>
2022-03-09 09:40:41 +0800

    添加 i18n国际化

--- src/pages/login/Login.vue
+++ src/pages/login/Login.vue
@@ -1,14 +1,22 @@
 <template>
-  <a>{{ count }}</a>
+  <h1>{{ $t(`message.Home`) }}</h1>
+  <div @click="change('zh')">中文</div>
+  <div @click="change('en')">English</div>
 </template>
 
 <script lang="ts">
-import { ref, defineComponent } from 'vue'
+import { defineComponent, getCurrentInstance } from 'vue'
+import { useI18n } from 'vue-i18n' //要在js中使用国际化
 export default defineComponent({
   name: 'LoginPage',
   setup: () => {
-    const count = ref(0)
-    return { count }
+    const { proxy } = getCurrentInstance()
+    function change(type: string) {
+      proxy.$i18n.locale = type
+    }
+    const { t } = useI18n()
+    console.log(t('message.pagesDes'))
+    return { change }
   },
 })
 </script>
