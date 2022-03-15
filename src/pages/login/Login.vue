ea926658cf3bd6788f68ca7d807edec614275d46
asian.kuang <asian.kuang@cn.shijigruop.com>
2022-03-15 09:05:20 +0800

    国际化本地

--- src/pages/login/Login.vue
+++ src/pages/login/Login.vue
@@ -1,19 +1,43 @@
 <template>
-  <h1>{{ $t(`message.Home`) }}</h1>
+  <h1>{{ t(`message.Home`) }}</h1>
   <div @click="change('zh')">中文</div>
   <div @click="change('en')">English</div>
+  <kyz></kyz>
 </template>
 
 <script lang="ts">
 import { defineComponent, getCurrentInstance } from 'vue'
+import { useI18n } from 'vue-i18n'
+
+const Kyz = {
+  template: `
+    <div>
+      <p>Component1 locale messages: {{ $t("message.hello") }}</p>
+    </div>
+  `,
+  i18n: {
+    messages: {
+      en: { message: { hello: 'hello component1' } },
+      zh: { message: { hello: 'こんにちは、component1' } },
+    },
+  },
+}
 export default defineComponent({
   name: 'LoginPage',
+  components: { kyz: Kyz },
   setup: () => {
     const { proxy } = getCurrentInstance() || {}
     function change(type: string) {
       proxy!.$i18n.locale = type
     }
-    return { change }
+    const { t } = useI18n({
+      fallbackLocale: 'zh',
+      messages: {
+        en: { message: { Home: 'hello component1' } },
+        zh: { message: { Home: 'こんにちは、component1' } },
+      },
+    })
+    return { change, t }
   },
 })
 </script>
--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
+      vue: 'vue/dist/vue.esm-bundler.js', // 定义vue的别名，如果使用其他的插件，可能会用到别名
     },
   },
   css: {
